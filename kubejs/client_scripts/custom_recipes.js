678f2afd7ef9cb21dbec9b2a326982a8aa2eb2d8
HarryBezaskie <mees.penners@gmail.com>
2022-09-08 23:52:35 +0200

    added kubejs

---
+++ kubejs/client_scripts/custom_recipes.js
@@ -0,0 +1,11 @@
+onEvent('recipes', event => {
+    event.shaped('6x minecraft:iron_ingot', [
+      'AAA',
+      'S S',
+      'AAA'
+    ], {
+      S: 'minecraft:coal',
+      A: 'mysticalagriculture:iron_essence'
+    })
+    event.remove({output: "minecraft:iron_ingot", mod: "Mystical agriculture"})
+  })
