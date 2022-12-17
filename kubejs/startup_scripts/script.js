24c179f1c6cda30dc7c7ae108df993b6af805d33
FooterMan14 | FM Studios <71946346+FooterMan14@users.noreply.github.com>
2022-12-17 11:14:54 -0500

    Add files via upload

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -1,13 +1,31 @@
-// priority: 0
-
-console.info('Hello, World! (You will only see this line once in console, during startup)')
-
 onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
+	event.create('compressednuclearwaste')
+	.displayName('Compressed Nuclear Waste')
+	.texture(compressednuclearwaste)
 })
-
-onEvent('block.registry', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+onEvent('worldgen.remove', event => {
+  event.removeOres(ores => {
+    ores.blocks = [ 'immersiveengineering:ore_copper',
+	'immersiveengineering:ore_aluminum',
+'immersiveengineering:ore_lead',
+'immersiveengineering:ore_silver',
+'immersiveengineering:ore_nickle',
+'immersiveengineering:ore_uranium',
+'thermal:cinnabar_ore',
+'thermal:niter_ore',
+'thermal:sulfur_ore',
+'thermal:copper_ore',
+'thermal:tin_ore',
+'thermal:lead_ore',
+'thermal:silver_ore',
+'thermal:nickle_ore',
+'thermal:oil_sand',
+'thermal:oil_red_sand',
+'tinkers_reforged:aluminum_ore',
+'tmechworks:aluminum_ore',
+'tmechworks:copper_ore',
+'powah:uranite_ore_poor',
+'powah:uranite_ore',
+'powah:uranite_ore_dense'	] // Ore Removal
+  })
 })
