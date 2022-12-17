eb3d5d8053a2549ac37eac6a6b4af312ee8e3df1
FooterMan14 | FM Studios <71946346+FooterMan14@users.noreply.github.com>
2022-12-17 11:20:09 -0500

    Update script.js

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,3 +3,29 @@ onEvent('item.registry', event => {
 	.displayName('Compressed Nuclear Waste')
 	.texture(compressednuclearwaste)
 })
+onEvent('worldgen.remove', event => {
+  event.removeOres(ores => {
+    ores.blocks = [ 'immersiveengineering:ore_copper',
+	'immersiveengineering:ore_aluminum',
+'immersiveengineering:ore_lead',
+'immersiveengineering:ore_silver',
+'immersiveengineering:ore_nickle',
+'immersiveengineering:ore_uranium',
+'thermal:cinnabar_ore',
+'thermal:niter_ore',
+'thermal:sulfur_ore',
+'thermal:copper_ore',
+'thermal:tin_ore',
+'thermal:lead_ore',
+'thermal:silver_ore',
+'thermal:nickle_ore',
+'thermal:oil_sand',
+'thermal:oil_red_sand',
+'tinkers_reforged:aluminum_ore',
+'tmechworks:aluminum_ore',
+'tmechworks:copper_ore',
+'powah:uranite_ore_poor',
+'powah:uranite_ore',
+'powah:uranite_ore_dense'	] // Ore Removal
+  })
+})
