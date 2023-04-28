d5d3037a7d0351558535f1b2fc19f2b966f8e0f0
FooterMan15 Dev <71946346+FooterMan15@users.noreply.github.com>
2023-04-27 21:26:16 -0400

    Client Updates

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,5 @@
+ClientEvents.highPriorityAssets(event => {
+    event.addLang('gas.kubejs.fluoridelyne', "Fluoridelyne")
+    event.addLang('gas.kubejs.monster_meka_coolant', "Monster Meka Coolant")
+    event.addLang('gas.kubejs.monster_waste', "Monster Waste")
+})
