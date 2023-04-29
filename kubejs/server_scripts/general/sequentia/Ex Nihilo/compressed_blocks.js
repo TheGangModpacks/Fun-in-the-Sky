797970dbb739700cd4ed92154ba2c456d484a897
ogmon1243 <83802482+ogmon1243@users.noreply.github.com>
2023-04-29 08:51:17 -0500

    Bug Fixes (#31)
    Bunch of random things

--- kubejs/server_scripts/general/sequentia/Ex Nihilo/compressed_blocks.js
+++ kubejs/server_scripts/general/sequentia/Ex Nihilo/compressed_blocks.js
@@ -1,25 +1,33 @@
 ServerEvents.recipes(event =>{
     event.shapeless("kubejs:compressed_cobblestone_x1", ['9x minecraft:cobblestone'])
     event.shapeless("9x minecraft:cobblestone", ['kubejs:compressed_cobblestone_x1'])
+
     event.shapeless("kubejs:compressed_cobblestone_x2", ['9x kubejs:compressed_cobblestone_x1'])
+    event.shapeless("9x kubejs:compressed_cobblestone_x1", ['kubejs:compressed_cobblestone_x2'])
+
     event.shapeless("kubejs:compressed_cobblestone_x3", ['9x kubejs:compressed_cobblestone_x2'])
-    event.shapeless("kubejs:compressed_cobblestone_x1", ['9x minecraft:cobblestone'])
-    event.shapeless("kubejs:compressed_cobblestone_x2", ['9x kubejs:compressed_cobblestone_x1'])
-    event.shapeless("kubejs:compressed_cobblestone_x3", ['9x kubejs:compressed_cobblestone_x3'])
+    event.shapeless("9x kubejs:compressed_cobblestone_x2", ['kubejs:compressed_cobblestone_x3'])
+
+
     event.shapeless("kubejs:compressed_sand_x1", ['9x minecraft:sand'])
     event.shapeless("9x minecraft:sand", ['kubejs:compressed_sand_x1'])
+
     event.shapeless("kubejs:compressed_sand_x2", ['9x kubejs:compressed_sand_x1'])
+    event.shapeless("9x kubejs:compressed_sand_x1", ['kubejs:compressed_sand_x2'])
+
     event.shapeless("kubejs:compressed_sand_x3", ['9x kubejs:compressed_sand_x2'])
-    event.shapeless("kubejs:compressed_sand_x1", ['9x minecraft:sand'])
-    event.shapeless("kubejs:compressed_sand_x2", ['9x kubejs:compressed_sand_x1'])
-    event.shapeless("kubejs:compressed_sand_x3", ['9x kubejs:compressed_sand_x2'])
+    event.shapeless("9x kubejs:compressed_sand_x2", ['kubejs:compressed_sand_x3'])
+
+
     event.shapeless("kubejs:compressed_gravel_x1", ['9x minecraft:gravel'])
     event.shapeless("9x minecraft:gravel", ['kubejs:compressed_gravel_x1'])
+
     event.shapeless("kubejs:compressed_gravel_x2", ['9x kubejs:compressed_gravel_x1'])
+    event.shapeless("9x kubejs:compressed_gravel_x1", ['kubejs:compressed_gravel_x2'])
+
     event.shapeless("kubejs:compressed_gravel_x3", ['9x kubejs:compressed_gravel_x2'])
-    event.shapeless("kubejs:compressed_gravel_x1", ['9x minecraft:gravel'])
-    event.shapeless("kubejs:compressed_gravel_x2", ['9x kubejs:compressed_gravel_x1'])
-    event.shapeless("kubejs:compressed_gravel_x3", ['9x kubejs:compressed_gravel_x2'])
+    event.shapeless("9x kubejs:compressed_gravel_x2", ['kubejs:compressed_gravel_x3'])
+
 
     event.custom({type: hammer, results:[Item.of('minecraft:gravel', 9).withChance(1.0).toJson()], input: Item.of("kubejs:compressed_cobblestone_x1")})
     event.custom({type: hammer, results:[Item.of('minecraft:gravel', 81).withChance(1.0).toJson()], input: Item.of("kubejs:compressed_cobblestone_x2")})
